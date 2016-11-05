0a4eef60caba81be7268b5dddbda65b598c2d7bc
andriyzherdiy <andriy.zherdiy@unity-bars.com>
2016-11-05 21:37:48 +0200

    Added examples for 5 6 7

--- lecture7/example/gulpfile.js
+++ lecture7/example/gulpfile.js
@@ -6,17 +6,17 @@ var watch = require('gulp-watch');
 var sass = require('gulp-sass');
  
 gulp.task('less', function () {
-  return gulp.src('./src/less/**/*.less')
+  return gulp.src('src/less/**/*.less')
     .pipe(less({
       paths: [ path.join(__dirname, 'less', 'includes') ]
     }))
-    .pipe(gulp.dest('./public/css'));
+    .pipe(gulp.dest('public/css'));
 });
  
 gulp.task('sass', function () {
   return gulp.src('./src/sass/**/*.scss')
     .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./css'));
+    .pipe(gulp.dest('./public/css'));
 });
 
 
